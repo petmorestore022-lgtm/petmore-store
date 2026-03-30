40bd236a242eb9d8aa0cb195bfbcb814af0cf2d2
bruno caramelo souza <bruno.caramelo5@gmail.com>
2026-03-30 18:11:24 -0300

    working on original theme

--- app/design/frontend/BzoTech/petshop_child/registration.php
+++
@@ -1,6 +0,0 @@
-<?php
-\Magento\Framework\Component\ComponentRegistrar::register(
-    \Magento\Framework\Component\ComponentRegistrar::THEME,
-    'frontend/BzoTech/petshop_child',
-    __DIR__
-);
